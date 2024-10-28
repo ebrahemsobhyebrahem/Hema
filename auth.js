1ae9f7cc557e444c3103ea0d20cc105eae2fc97b
ebrahemsobhyebrahem <ebrahimebrahem60@gmail.com>
2024-10-28 18:10:59 +0300

    auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ import { auth, db } from './firebaseConfig.js';
 import { GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js';
 import { setDoc, doc } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
 
-function signInWithGoogle() {
+export function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
         .then(async (result) => {
@@ -15,12 +15,16 @@ function signInWithGoogle() {
                 email: user.email,
                 profile_picture: user.photoURL
             });
+
             console.log("User data saved successfully!");
+
+            // إخفاء زر تسجيل الدخول وإظهار حالة تسجيل الدخول
+            document.querySelector('.signin-button').style.display = 'none';
+            const loginStatus = document.querySelector('.login-status');
+            loginStatus.innerHTML = `Logged in as ${user.displayName}`;
+            loginStatus.style.display = 'block';
         })
         .catch((error) => {
             console.error("Error during sign in: ", error);
         });
 }
-
-// جعل الدالة متاحة في النطاق العام
-window.signInWithGoogle = signInWithGoogle;
