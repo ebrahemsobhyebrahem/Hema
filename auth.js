fcde96f00a4f66608358b05a35fbd257aafb390a
ebrahemsobhyebrahem <ebrahimebrahem60@gmail.com>
2024-10-29 00:35:52 +0300

    auth.js

--- auth.js
+++ auth.js
@@ -1,30 +1,33 @@
 // auth.js
 import { auth, db } from './firebaseConfig.js';
-import { GoogleAuthProvider, signInWithRedirect } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js';
+import { GoogleAuthProvider, signInWithRedirect, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js';
 import { setDoc, doc } from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
 
 function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider)  // استخدام signInWithRedirect هنا
-        .then(async (result) => {
-            const user = result.user;
-
-            // تخزين بيانات المستخدم في Firestore
-            await setDoc(doc(db, "users", user.uid), {
-                name: user.displayName,
-                email: user.email,
-                profile_picture: user.photoURL
-            });
-            console.log("User data saved successfully!");
-
-            // إخفاء زر تسجيل الدخول وإظهار حالة تسجيل الدخول
-            document.getElementById('login-button-container').style.display = 'none';
-            document.getElementById('login-status').style.display = 'block';
-        })
-        .catch((error) => {
-            console.error("Error during sign in: ", error);
-        });
+    signInWithRedirect(auth, provider);
 }
 
-// جعل الدالة متاحة في النطاق العام
+// تحقق من حالة تسجيل الدخول عند تحميل الصفحة
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        // إظهار حالة تسجيل الدخول
+        document.getElementById('login-button-container').style.display = 'none';
+        document.getElementById('login-status').style.display = 'block';
+
+        // تخزين بيانات المستخدم إذا لم تكن موجودة
+        await setDoc(doc(db, "users", user.uid), {
+            name: user.displayName,
+            email: user.email,
+            profile_picture: user.photoURL
+        }, { merge: true });
+
+        console.log("User is logged in and data is saved.");
+    } else {
+        // إظهار زر تسجيل الدخول
+        document.getElementById('login-button-container').style.display = 'block';
+        document.getElementById('login-status').style.display = 'none';
+    }
+});
+
 window.signInWithGoogle = signInWithGoogle;
